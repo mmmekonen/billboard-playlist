271f0d3c2f005fd0e38ae5f1fd68a992b7ac7daa
mmmekonen <maelat.mekonen@gmail.com>
2023-07-12 20:11:58 -0400

    added spotify code to search songs and make playlist

--- billboardsongs.py
+++ billboardsongs.py
@@ -2,8 +2,41 @@ from datetime import date, timedelta
 import billboard
 import sys
 import json
+import spotipy
 
 AUTH_FILE = 'authorization.json'
+
+
+def makePlaylist(year):
+    songs = makeList(year)
+    print('Got songs from chart')
+
+    auth = loadAuth()
+    sp = spotipy.Spotify(auth_manager=spotipy.oauth2.SpotifyPKCE(
+        client_id=auth['client_id'],
+        scope=auth['scope'],
+        redirect_uri=auth['redirect_uri']))
+
+    uris = findURIs(sp, songs)
+    print('Found URIs')
+
+    user_id = sp.me()['id']
+    playlist = sp.user_playlist_create(user_id, str(year))
+
+    for n in range(0, len(uris), 100):
+        sp.playlist_add_items(playlist_id=playlist['id'], items=uris[n:n + 100])
+    print('Made playlist')
+
+
+def findURIs(sp, songs):
+    uris = []
+    for song in songs:
+        try:
+            uris.append(
+                sp.search(q=song, type='track')['tracks']['items'][0]['uri'])
+        except IndexError:
+            print(f"Couldn't fetch {song}.")
+    return uris
 
 
 def makeList(year):
@@ -12,12 +45,12 @@ def makeList(year):
         chart = billboard.ChartData('hot-100', date)
         tracks.update(
             {str(song) for song in chart.entries if str(song) not in tracks})
-    print(*sorted(tracks), sep='\n')
+    return tracks
 
 
 def loadAuth():
     with open(AUTH_FILE) as jsonFile:
-        auth = json.loads(jsonFile)
+        auth = json.load(jsonFile)
     return auth
 
 
@@ -31,10 +64,5 @@ def allSaturdays(year):
 
 
 if __name__ == '__main__':
-    try:
-        year = int(sys.argv[1])
-        makeList(year)
-    except IndexError:
-        print(f'Must include year: python {sys.argv[0]} <YYYY>')
-    except ValueError:
-        print('Year must be a number in the format YYYY.')
+    year = int(sys.argv[1])
+    makePlaylist(year)
